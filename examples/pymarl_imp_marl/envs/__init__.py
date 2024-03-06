808441ebf69df11760f19a5747fe7ef41c96d3bf
moratodpg <moratosotelo@gmail.com>
2024-03-06 21:19:11 +0100

    remove config env files and update road env

--- examples/pymarl_imp_marl/envs/__init__.py
+++ examples/pymarl_imp_marl/envs/__init__.py
@@ -1,14 +1,11 @@
 from functools import partial
 
-from imp_marl.imp_wrappers.pymarl_wrapper.MultiAgentEnv import MultiAgentEnv
-from imp_marl.imp_wrappers.pymarl_wrapper.pymarl_wrap_ma_struct import PymarlMAStruct
-from imp_marl.imp_wrappers.pymarl_wrapper.pymarl_wrap_sa_struct import PymarlSAStruct
-
+from envs.MultiAgentEnv import MultiAgentEnv
+from envs.pymarl_ma import PymarlMARoadEnv
 
 def env_fn(env, **kwargs) -> MultiAgentEnv:
     return env(**kwargs)
 
 
 REGISTRY = {}
-REGISTRY["struct_marl"] = partial(env_fn, env=PymarlMAStruct)
-REGISTRY["struct_sarl"] = partial(env_fn, env=PymarlSAStruct)
+REGISTRY["road_marl"] = partial(env_fn, env=PymarlMARoadEnv)
